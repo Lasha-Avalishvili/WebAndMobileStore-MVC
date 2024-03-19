777967d09a82afcd12706c843805d2ef4e1cd91d
lavalishvili <lavalishvili@LHO10048790>
2024-03-20 01:38:39 +0400

    Add project files.

---
+++ WebAndMobileStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAndMobileStore.Models;
+
+namespace WebAndMobileStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            var items = new List<Item>
+    {
+        new Item { Name = "Item 1", Description = "Description of Item 1", Price = 10.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 2", Description = "Description of Item 2", Price = 15.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 3", Description = "Description of Item 3", Price = 20.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 1", Description = "Description of Item 1", Price = 10.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 2", Description = "Description of Item 2", Price = 15.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 3", Description = "Description of Item 3", Price = 20.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 1", Description = "Description of Item 1", Price = 10.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 2", Description = "Description of Item 2", Price = 15.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" },
+        new Item { Name = "Item 3", Description = "Description of Item 3", Price = 20.99m, ImageUrl = "https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" }
+    };
+
+            return View(items);
+        }
+
+
+
+    }
+}
---
+++ WebAndMobileStore/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAndMobileStore.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
